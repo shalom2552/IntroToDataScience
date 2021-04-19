2caddfa2a071dc760328545efc18511458bab24b
marshall <shalom2552@gmail.com>
2021-04-19 15:16:38 +0300

    lab1 finish

--- lab1/src/hw1_lab_206320772_id2.sh
+++ lab1/src/hw1_lab_206320772_id2.sh
@@ -23,10 +23,15 @@ whatever when whence whenever where whereafter whereas whereby wherein
 whereupon wherever whether which while whither who whoever whole whom whose
 why will with within without would yet you your yours yourself yourselves )
 
-# Alice book to lower case 
-awk '{print tolower($0)}' Alice_book | tr -cd '\ \t\n[:alnum:]' > Alice_book_clean.txt
+# Transforming text to lower case 
+awk '{print tolower($0)}' Alice_book | tr -cd '\ \t\n[:alnum:]' > Alice_book_tolower.txt
 
-# remove stop words 
+# Cleaning begin trash
+awk 'BEGIN{a=0}{if ($1 == "chapter"){ a=1}
+	if (a == 1){print $0}}' Alice_book_tolower.txt > Alice_book_clean.txt
+rm -f Alice_book_tolower.txt
+
+# Removing "stop" words 
 for i in ${stop_words[@]};
 	do
 		sed -i "s/\<$i\>//g" Alice_book_clean.txt
@@ -36,7 +41,7 @@ done
 sed '/^[[:space:]]*$/d' -i Alice_book_clean.txt
 sed "s/^[ \t]*//" -i Alice_book_clean.txt
 
-# splite book to chapters
+# spliting book into chapters
 rm -rf chapters
 mkdir chapters
 awk 'BEGIN{RS="chapter "}{
@@ -44,7 +49,7 @@ awk 'BEGIN{RS="chapter "}{
 		{print > "chapters/chapter "(NR-1);}
 	}' Alice_book_clean.txt
 
-# find the most common pair and the Most common first word in book
+# Finding the most common pair and the Most common first word in book
 rm -f output.txt
 touch output.txt
 awk '{
@@ -62,7 +67,7 @@ awk '{
 		print "Most common first word in the book:",m
 	}' Alice_book_clean.txt >> output.txt
 
-# find the most common pair and the Most common first word in each chapter
+# Finding the most common pair and the Most common first word in each chapter
 cd chapters
 for n in {1..12};
 do
@@ -74,26 +79,27 @@ do
 		}END {
 			for (words in a)
 				if (a[words] > a[m]) m = words
-			print "Most common pair in",FILENAME":",m;
+			print "Most common pair for",FILENAME":",m;
 			for (words in b)
 				if (b[words] > b[m]) m = words
-			print "Most common first word in",FILENAME":",m
+			print "Most common first word for",FILENAME":",m
 		}' "chapter "$n >> ../output.txt 
 done 
 cd ..
 
-# avg row length
+# Average row length
 awk 'BEGIN{sum=0}
 	{sum+=NF}END{print "Average line length:",sum/NR
 }' Alice_book_clean.txt >> output.txt
 
-# number of lines shorter then the avg
+# Number of lines shorter than the average
+sed -i "s/chapter//g" Alice_book_clean.txt
 awk 'BEGIN{sum=0}
 	{if (NF <= 4) sum+=1}
 	END{print "Number of lines shorter than the average:",sum
 }' Alice_book_clean.txt >> output.txt
 
-# alice avg position in row
+# alice average position in row
 awk 'BEGIN{pos=0;indicator=0;count=0}
     {n=1;for(i=1;i<=NF;i++){
         if($i == "alice"){pos = pos + n;indicator=1}n++;}
@@ -102,6 +108,6 @@ awk 'BEGIN{pos=0;indicator=0;count=0}
     END{print "Average place of Alice in a line: "pos/count
 }' Alice_book_clean.txt >> output.txt
 
-# remove all trash leftovers
+# removing all trash leftovers
 rm -rf chapters Alice_book_clean.txt
 cat output.txt
